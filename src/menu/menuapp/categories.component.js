4a468a09d564a1b0cb4d78e3efefcc03c401c7c0
wbricenos <51423444+wbricenos@users.noreply.github.com>
2019-07-31 09:41:16 +0200

    Delete categories.component.js

--- src/menu/menuapp/categories.component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  angular.module('MenuApp')
-  .component('categoires', {
-    templateUrl: 'src/menu/menuapp/templates/categories.template.html',
-    bindings: {
-      categoriesList: '<'
-    }
-  })
-})();
